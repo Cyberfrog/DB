ef3f386ab8da6cd8445ace7a09c02f3e88ad5a60
Cyberfrog <Cyberfrog@users.noreply.github.com>
2015-03-11 08:04:01 +0530

    Step_app : centers and education details created

--- step_script.sql
+++ step_script.sql
@@ -36,6 +36,13 @@ pin_code	VARCHAR(10),
 CONSTRAINT aplicant_pk PRIMARY KEY(id),
 CONSTRAINT aplicant_gender_chk CHECK(gender IN ('M', 'F') )
 );
+CREATE TABLE Centers (
+code  VARCHAR(10),
+Name 	VARCHAR(10),
+adderess	VARCHAR(100),
+city 		VARCHAR(100),
+CONSTRAINT apti_test_pk PRIMARY KEY(center_code),
+) TABLESPACE ts_step_system; 
 
 CREATE TABLE Apti_test (
 id 		 	 Number(10),
@@ -43,6 +50,17 @@ Applicant_id NUMBER(10),
 center_code	VARCHAR(50),
 Exam_date	DATE,
 CONSTRAINT apti_test_pk PRIMARY KEY(id),
-CONSTRAINT Applicant_id_fk FOREIGN KEY (Applicant_id) REFERENCES Aplicant(id)
+CONSTRAINT Applicant_id_fk FOREIGN KEY (Applicant_id) REFERENCES Aplicant(id),
+CONSTRAINT center_code_fk FOREIGN KEY (center_code) REFERENCES centers(code),
 ) TABLESPACE ts_step_system; 
 
+CREATE TABLE Educational_Details(
+Enrollment_number Number(10),
+Applicant_id NUMBER(10),
+Univercity	VARCHAR(50)NOT NULL,
+10th_Marks	NUMBER(3)NOT NULL,
+12th_Marks  NUMBER(3),
+Diploma_Marks NUMBER(3)NOT NULL,	
+CONSTRAINT apti_test_pk PRIMARY KEY(Enrollment_number),
+CONSTRAINT Applicant_id_fk FOREIGN KEY (Applicant_id) REFERENCES Aplicant(id)
+) TABLESPACE ts_step_system; 
